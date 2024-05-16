26734e93a27e28da6d4f31ffe2fecdbbd31d56a9
AnjaliJivani <145247343+AnjaliJivani@users.noreply.github.com>
2024-05-16 12:14:29 +0530

    Add files via upload

---
+++ A_1_lab14.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+	int a,i,n;
+	int arr[i];
+	printf("enter n ");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++){
+		printf("enter an element in arr[%d]",i);
+		scanf("%d",&arr[i]);
+	}
+	for(i=n-1;i>n;i--){
+		printf("%d",arr[i]);
+		
+	}
+}
