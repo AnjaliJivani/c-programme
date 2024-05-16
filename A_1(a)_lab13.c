45ef69df7ac597129116b0a56b932c3d7de830c6
AnjaliJivani <145247343+AnjaliJivani@users.noreply.github.com>
2024-05-16 12:12:45 +0530

    Add files via upload

---
+++ A_1(a)_lab13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int n,i,a=1,b,x;
+	//printf("enter x");
+	//scanf("%d",&x);
+	for(i=5;i>=0;i--){
+		for(n=1;n<=i;n++){
+		//	printf("%d",i);
+		printf("*");
+		}
+		printf("\n");
+		//a++;
+	}
+}
